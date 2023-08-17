055b1f9138bb8b6c6354c6335462c934aed361cb
brighteast99 <brighteast99@gmail.com>
2023-08-17 23:17:18 +0900

    🧱 솔루션 저장 형식 변경
    문제 이름.js에 주석+TC+solution 함수 모두 작성하는 대신
    문제 이름 디렉토리에 info.md, testCases.json, solution.js로 분리하여 작성

---
+++ programmers/2019 KAKAO BLIND RECRUITMENT/오픈채팅방/solution.js
@@ -0,0 +1,24 @@
+export function solution({ record }) {
+	let users = {};
+	let answer = [];
+
+	for (let i = 0; i < record.length; i++) {
+		let [, id, name] = record[i].split(" ");
+		if (name) users[id] = name;
+	}
+
+	for (let i = 0; i < record.length; i++) {
+		let [type, id] = record[i].split(" ");
+		switch (type) {
+			case "Enter":
+				answer.push(`${users[id]}님이 들어왔습니다.`);
+				break;
+			case "Leave":
+				answer.push(`${users[id]}님이 나갔습니다.`);
+				break;
+			default:
+				continue;
+		}
+	}
+	return answer;
+}
---
+++ programmers/Summer ∕ Winter Coding(~2018)/영어 끝말잇기/solution.js
@@ -0,0 +1,19 @@
+export function solution({ n, words }) {
+	var answer = [0, 0];
+	let dict = {};
+
+	dict[words[0]] = true;
+	let last = words[0].slice(-1);
+	for (let i = 1; i < words.length; i++) {
+		if (dict[words[i]] || words[i].slice(0, 1) != last) {
+			answer[0] = (i % n) + 1;
+			answer[1] = parseInt(i / n) + 1;
+			break;
+		}
+
+		last = words[i].slice(-1);
+		dict[words[i]] = true;
+	}
+
+	return answer;
+}
