e6873ca5a78158ba08404cd462d38d629c21e14a
brighteast99 <brighteast99@gmail.com>
2023-08-20 23:05:04 +0900

    ✅ 금과 은 운반하기@프로그래머스/월간 코드 챌린지 시즌3
    https://school.programmers.co.kr/learn/courses/30/lessons/86053

---
+++ problems/programmers/월간 코드 챌린지 시즌3/금과 은 운반하기/solution.js
@@ -0,0 +1,27 @@
+function canBuild(a, b, g, s, w, t, T) {
+	let [S, G, total] = [0, 0, 0];
+
+	t.forEach((t, i) => {
+		let W = Math.min(g[i] + s[i], w[i] * Math.round(T / (2 * t)));
+
+		S += Math.min(s[i], W);
+		G += Math.min(g[i], W);
+		total += W;
+	});
+
+	return a <= G && b <= S && a + b <= total;
+}
+
+function solution({ a, b, g, s, w, t }) {
+	let left = 1;
+	let right = Math.max(...t) * (a + b) * 2;
+
+	while (left <= right) {
+		let mid = Math.floor((left + right) / 2);
+
+		if (canBuild(a, b, g, s, w, t, mid)) right = mid - 1;
+		else left = mid + 1;
+	}
+
+	return left;
+}
