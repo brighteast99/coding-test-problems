5ba75dd015809a2e2b04d8fe007a0759d0597121
brighteast99 <brighteast99@gmail.com>
2023-08-20 23:39:53 +0900

    ⚰️ 사용하지 않는 매개변수 제거 🔇 로그 제거
    size를 이용한 인덱스 체크 -> undefined 여부 체크로 변경 과정에서
    불필요하게 남아있던 size 매개변수 제거
    size 로그 제거

--- problems/programmers/연습문제/무인도 여행/solution.js
+++ problems/programmers/연습문제/무인도 여행/solution.js
@@ -1,4 +1,4 @@
-function dfs(map, size, x, y) {
+function dfs(map, x, y) {
 	if (!map[y]?.[x]?.days) return 0;
 	if (map[y][x].visited) return 0;
 
@@ -6,10 +6,10 @@ function dfs(map, size, x, y) {
 
 	return (
 		map[y][x].days +
-		dfs(map, size, x - 1, y) +
-		dfs(map, size, x + 1, y) +
-		dfs(map, size, x, y - 1) +
-		dfs(map, size, x, y + 1)
+		dfs(map, x - 1, y) +
+		dfs(map, x + 1, y) +
+		dfs(map, x, y - 1) +
+		dfs(map, x, y + 1)
 	);
 }
 
@@ -19,7 +19,6 @@ function solution({ maps }) {
 		x: maps[0].length,
 		y: maps.length,
 	};
-	console.log(size);
 	let map = maps.map((map) =>
 		Array.from(map).map((v) => {
 			return {
@@ -31,7 +30,7 @@ function solution({ maps }) {
 
 	for (let y = 0; y < size.y; y++)
 		for (let x = 0; x < size.x; x++) {
-			const island = dfs(map, size, x, y);
+			const island = dfs(map, x, y);
 			if (island) islands.push(island);
 		}
 
