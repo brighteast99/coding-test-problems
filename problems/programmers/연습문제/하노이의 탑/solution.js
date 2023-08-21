f02fd7373f3eac37a4edc2b5c88887122f475fe9
brighteast99 <brighteast99@gmail.com>
2023-08-21 15:34:43 +0900

    ✅ 하노이의 탑@프로그래머스/연습문제
    https://school.programmers.co.kr/learn/courses/30/lessons/12946

---
+++ problems/programmers/연습문제/하노이의 탑/solution.js
@@ -0,0 +1,17 @@
+function move(n, from, by, to, moves) {
+	if (n === 1) {
+		moves.push([from, to]);
+		return;
+	}
+	move(n - 1, from, to, by, moves);
+	moves.push([from, to]);
+	move(n - 1, by, from, to, moves);
+}
+
+function solution({ n }) {
+	var answer = [];
+
+	move(n, 1, 2, 3, answer);
+
+	return answer;
+}
