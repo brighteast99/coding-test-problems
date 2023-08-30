4aa980ed4ef746b47c90450be13e5d21c99e49ff
brighteast99 <brighteast99@gmail.com>
2023-08-30 11:53:01 +0900

    ✅ 매출 하락 최소화@프로그래머스/2021 KAKAO BLIND RECRUITMENT
    https://school.programmers.co.kr/learn/courses/30/lessons/72416
    
    🚧 매출 하락 최소화@프로그래머스/2021 KAKAO BLIND RECRUITMENT
    속도 개선했지만 시간초과
    
    🚧 매출 하락 최소화@프로그래머스/2021 KAKAO BLIND RECRUITMENT
    시간초과

---
+++ problems/programmers/2021 KAKAO BLIND RECRUITMENT/매출 하락 최소화/solution.js
@@ -0,0 +1,31 @@
+function dfs(sales, team, teams) {
+  if (typeof team !== 'object') return [0, sales[team - 1]]
+
+  let subCosts = []
+  let minSubCost = 0
+  let allAbsent = 0
+  let candidateCost = Number.MAX_SAFE_INTEGER
+  team.crews.forEach(leader => {
+    let temp = dfs(sales, teams[leader] || leader, teams)
+    subCosts.push(temp)
+    minSubCost += Math.min(...temp)
+    allAbsent += temp[0]
+    if (temp[1] - temp[0] < candidateCost) candidateCost = temp[1] - temp[0]
+  })
+
+  let leaderPresent = sales[team.leader - 1] + minSubCost
+  let leaderAbsent = minSubCost
+  if (leaderAbsent == allAbsent) leaderAbsent += candidateCost
+
+  return [leaderAbsent, leaderPresent]
+}
+function solution({ sales, links }) {
+  let teams = {}
+
+  links.forEach(([leader, crew]) => {
+    teams[leader] ??= { leader: leader, crews: [] }
+    teams[leader].crews.push(crew)
+  })
+
+  return Math.min(...dfs(sales, teams[1], teams))
+}
