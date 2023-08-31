9e04cdf3bfa83bfdaa7d871a804beafce701c502
brighteast99 <brighteast99@gmail.com>
2023-08-31 10:14:21 +0900

    ✨ 문제 생성시 README.md 일부 자동 기입
    create로 문제 생성시 README.md에 문제 이름, 분류 자동 기입
    
    resolves #4

--- actions/manage.js
+++ actions/manage.js
@@ -1,6 +1,15 @@
 import fs from 'fs'
 import path from 'path'
 
+function initREADME(readMe, problemPath) {
+  let tokens = problemPath.split('/')
+  readMe = readMe
+    .replace('문제 이름', tokens[tokens.length - 2])
+    .replace('문제 분류', tokens[tokens.length - 3])
+  const filePath = path.join(problemPath, 'README.md')
+  fs.writeFileSync(filePath, readMe)
+}
+
 export function initSolution(problemPath) {
   if (fs.existsSync(problemPath)) throw new Error('Problem already exists')
 
@@ -8,6 +17,13 @@ export function initSolution(problemPath) {
 
   let files = fs.readdirSync('./problems/template/')
   files.forEach(file => {
+    if (file.includes('.md')) {
+      initREADME(
+        fs.readFileSync(path.join('./problems/template/', file), 'utf8'),
+        problemPath
+      )
+      return
+    }
     const filePath = path.join('./problems/template/', file)
     const destFilePath = path.join(problemPath, file)
     fs.copyFileSync(filePath, destFilePath)
