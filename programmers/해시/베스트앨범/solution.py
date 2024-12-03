2601b539517b5f30129b4da91baa838774fd2988
brighteast99 <brighteast99@gmail.com>
2024-12-04 01:12:44 +0900

    🐛 베스트앨범@프로그래머스/해시
    코드 오류 수정
    
    https://school.programmers.co.kr/learn/courses/30/lessons/42579
    
    Related to: #31

--- programmers/해시/베스트앨범/solution.py
+++ programmers/해시/베스트앨범/solution.py
@@ -1,20 +1,18 @@
 def solution(genres, plays):
     genre_statistics = dict()
-    n_musics = len(genres)
 
-    for i in range(n_musics):
-        genre = genres[i]
+    for i, (genre, n_played) in enumerate(zip(genres, plays)):
         if genre in genre_statistics:
-            genre_statistics[genre]["plays"] += plays[i]
+            genre_statistics[genre]["plays"] += n_played
             genre_statistics[genre]["musics"].append(i)
         else:
-            genre_statistics[genre] = {"plays": plays[i], "musics": [i]}
+            genre_statistics[genre] = {"plays": n_played, "musics": [i]}
 
-    genre_names = genre_statistics.keys()
-    genre_names.sort(key=lambda name: genre_statistics[name]["plays"], reverse=True)
+    genre_names = list(genre_statistics.keys())
+    genre_names.sort(key=lambda name: -genre_statistics[name]["plays"])
     answer = []
     for genre in genre_names:
-        genre_statistics[genre]["musics"].sort(key=lambda num: (plays[num], num))
+        genre_statistics[genre]["musics"].sort(key=lambda num: (-plays[num], num))
         answer.extend(genre_statistics[genre]["musics"][:2])
 
     return answer
