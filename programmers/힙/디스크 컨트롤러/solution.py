fb3da4c3e072166997a6598a3463b302814e7680
brighteast99 <brighteast99@gmail.com>
2024-12-05 22:40:59 +0900

    ✅ 디스크 컨트롤러@프로그래머스/힙
    https://school.programmers.co.kr/learn/courses/30/lessons/42627
    
    Resolves: #36

---
+++ programmers/힙/디스크 컨트롤러/solution.py
@@ -0,0 +1,39 @@
+import heapq
+
+
+class Job:
+    def __init__(self, time, workload):
+        self.time = time
+        self.workload = workload
+
+    def __lt__(self, other):
+        if self.workload != other.workload:
+            return self.workload < other.workload
+        if self.time != other.time:
+            return self.time < other.time
+        return False
+
+    def __repr__(self):
+        return str((self.id, self.time, self.workload))
+
+
+def solution(jobs):
+    jobs.sort()
+    jobs = [Job(s, l) for (s, l) in jobs]
+    task_queue = []
+    completed = []
+
+    t = 0
+    while len(task_queue) + len(jobs) > 0:
+        if len(task_queue) > 0:
+            cur_job = heapq.heappop(task_queue)
+        else:
+            cur_job = jobs.pop(0)
+        t = max(t, cur_job.time)
+        t += cur_job.workload
+        completed.append(t - cur_job.time)
+
+        while len(jobs) and t >= jobs[0].time:
+            heapq.heappush(task_queue, jobs.pop(0))
+
+    return sum(completed) // len(completed)
