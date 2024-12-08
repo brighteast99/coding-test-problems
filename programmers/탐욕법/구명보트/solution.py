45d5f61f6e13542d96d2c6e6e280d6bbc6e71a94
brighteast99 <brighteast99@gmail.com>
2024-12-08 23:16:13 +0900

    ♻️ 구명보트@프로그래머스/탐욕법
    deque을 이용한 풀이 추가
    https://school.programmers.co.kr/learn/courses/30/lessons/42885
    
    Related to: #48

--- programmers/탐욕법/구명보트/solution.py
+++ programmers/탐욕법/구명보트/solution.py
@@ -10,3 +10,21 @@ def solution(people, limit):
         answer += 1
 
     return answer
+
+
+# Solution using deque
+# from collections import deque
+
+
+# def solution(people, limit):
+#     people.sort(reverse=True)
+#     dq = deque(people)
+#     answer = 0
+#     while len(dq):
+#         if len(dq) > 1 and dq[0] + dq[-1] <= limit:
+#             dq.pop()
+
+#         dq.popleft()
+#         answer += 1
+
+#     return answer
