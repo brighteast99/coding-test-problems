7eacf2f440e0c8a4a862329136b5ae35944f7fcc
brighteast99 <brighteast99@gmail.com>
2025-01-14 00:07:56 +0900

    🔇 새로운 게임 2@boj/삼성 SW 역량 테스트 기출
    주석처리된 디버그 출력 삭제
    https://www.acmicpc.net/problem/17837
    
    Related to: #90

--- boj/삼성 SW 역량 테스트 기출/새로운 게임 2/solution.py
+++ boj/삼성 SW 역량 테스트 기출/새로운 게임 2/solution.py
@@ -2,21 +2,6 @@ import sys
 
 DIRECTION_VECTORS = [(1, 0), (0, -1), (-1, 0), (0, 1)]
 WHITE, RED, BLUE = 0, 1, 2
-colors = {0: "", 1: "\033[31m", 2: "\033[36m"}
-
-dir_string = {0: "→", 1: "↑", 2: "←", 3: "↓"}
-
-
-def print_board(board, pieces):
-    for row in board:
-        for cell in row:
-            print(
-                f"{colors[cell[0]]}{list(map(lambda piece: f'{piece} {dir_string[pieces[piece][1]]}', cell[1]))}\033[0m",
-                end=" ",
-            )
-        print()
-    print()
-
 
 n, k = map(int, sys.stdin.readline().split())
 board = [
@@ -35,16 +20,12 @@ for num in range(k):
     pieces[num] = [(x - 1, y - 1), direction]
     board[y - 1][x - 1][1].append(num)
 
-# print(pieces)
-# print_board(board, pieces)
 
 time = 0
 while time < 1000:
     time += 1
-    # print(f"----------------{time}------------------")
     for num in range(k):
         (x, y), direction = pieces[num]
-        # print("move", num, f"({x}, {y})", dir_string[direction])
 
         piece_idx = board[y][x][1].index(num)
         stack = board[y][x][1][piece_idx:]
@@ -55,14 +36,12 @@ while time < 1000:
             not (0 <= x + dx < n and 0 <= y + dy < n)
             or board[y + dy][x + dx][0] == BLUE
         ):
-            # print("flip direction")
             direction = (direction + 2) % 4
             dx, dy = DIRECTION_VECTORS[direction]
             if (
                 not (0 <= x + dx < n and 0 <= y + dy < n)
                 or board[y + dy][x + dx][0] == BLUE
             ):
-                # print("stuck")
                 dx, dy = 0, 0
         if board[y + dy][x + dx][0] == RED:
             if dx != 0 or dy != 0:
@@ -76,8 +55,6 @@ while time < 1000:
         board[y + dy][x + dx][1].extend(stack)
         if len(board[y + dy][x + dx][1]) >= 4:
             break
-        # print_board(board, pieces)
-        # sys.stdin.readline()
     else:
         continue
     break
