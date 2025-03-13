06777d8aeb07cea0a55345a9c0bea89a64ac9f3c
brighteast99 <brighteast99@naver.com>
2025-03-14 01:24:40 +0900

    ✅ 조건에 맞는 개발자 찾기@프로그래머스/SQL 고득점 Kit
    https://school.programmers.co.kr/learn/courses/30/lessons/276034
    
    Resolves: #132

---
+++ programmers/SQL 고득점 Kit/조건에 맞는 개발자 찾기/solution.sql
@@ -0,0 +1,19 @@
+SELECT DISTINCT
+    DEVELOPERS.ID,
+    DEVELOPERS.EMAIL,
+    DEVELOPERS.FIRST_NAME,
+    DEVELOPERS.LAST_NAME
+FROM
+    DEVELOPERS,
+    (
+        SELECT
+            CODE
+        FROM
+            SKILLCODES
+        WHERE
+            NAME IN ('Python', 'C#')
+    ) AS target_skills
+WHERE
+    DEVELOPERS.SKILL_CODE&target_skills.CODE
+ORDER BY
+    DEVELOPERS.ID
